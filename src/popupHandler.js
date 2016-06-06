e4b69caf8b39b0500b4c3c938bc2a1b293a765fc
dima_goncharuk_farrosoft <dmitry.goncharuk@farrosoft.com>
2016-06-06 13:13:18 +0300

    changed constructor

--- src/popupHandler.js
+++ src/popupHandler.js
@@ -1,209 +1,213 @@
 function PopupHandler () {
-    this.triggerAttribute = 'data-popup';
-    this.additionalDataAttributes = [];
-    this.deferredTriggerAttribute = 'data-deferred-popup';
-    this.disabledFormClass = 'js-disabled';
-    this.popupClass = 'b-popup';
-    this.popupWrapperClass = this.popupClass + '__wrapper';
-    this.popupCloseSelector = '[data-popup-close]';
-    this.popupContents = {};
-    this.popupHandlers = {};
-    this.focusOnFirstInput = true;
-    this.closeOnWrapperClick = true;
+    if ( this instanceof PopupHandler ) {
+        this.triggerAttribute = 'data-popup';
+        this.additionalDataAttributes = [];
+        this.deferredTriggerAttribute = 'data-deferred-popup';
+        this.disabledFormClass = 'js-disabled';
+        this.popupClass = 'b-popup';
+        this.popupWrapperClass = this.popupClass + '__wrapper';
+        this.popupCloseSelector = '[data-popup-close]';
+        this.popupContents = {};
+        this.popupHandlers = {};
+        this.focusOnFirstInput = true;
+        this.closeOnWrapperClick = true;
 
-    this.init = function ( settings ) {
-        if ( settings !== undefined ) {
-            for ( var setting in settings ) {
-                this[setting] = settings[setting];
-            }
-        }
-
-        this.getPopupsContent();
-        this.injectPopup();
-        this.initEventListeners();
-    };
-
-    this.getPopupsContent = function () {
-
-        var $this = this;
-        var ajaxRequestData = this.getAjaxRequestData();
-        if ( Object.keys(ajaxRequestData.popupRequestData).length !== 0 ) {
-            $.ajax({
-                url: themeVars.ajaxUrl,
-                type: "POST",
-                data: ajaxRequestData,
-                success: function ( response ) {
-                    if ( response != "no content" ) {
-                        response = $.parseJSON(response);
-                        for ( var popupType in response ) {
-                            $this.popupContents[popupType] = {
-                                formID: response[popupType].formID,
-                                content: response[popupType].content
-                            };
-                        }
-                    } else {
-                        console.log(response);
-                    }
-                },
-                error: function ( response ) {
-                    console.log(response);
-                }
-            });
-        }
-    };
-
-    this.fillPopup = function ( popupType ) {
-        var $this = this;
-        this.popup.html(this.popupContents[popupType].content);
-        this.getPopupsContent();
-        $('form#' + this.popupContents[popupType].formID).submit(function ( event ) {
-            event.preventDefault();
-            var currentForm = $(this);
-
-            if ( !currentForm.hasClass($this.disabledFormClass) ) {
-                $this.formSubmission($this, popupType, currentForm);
-            }
-        });
-    };
-
-    this.formSubmission = function ( popupHandler, handlerType, form ) {
-        if ( this.popupHandlers[handlerType] !== undefined ) {
-            this.popupHandlers[handlerType](form, popupHandler);
-        } else {
-            console.log('formSubmission-default');
-            console.log(handlerType);
-        }
-    };
-
-    this.showPopup = function ( popupType, defer ) {
-        if ( typeof popupType !== 'string' ) {
-            defer = defer === undefined ? false : defer;
-            var attr = defer ? this.deferredTriggerAttribute : this.triggerAttribute;
-            popupType = popupType.attr(attr);
-        }
-        this.hidePopup();
-
-        if ( this.popupContents[popupType] !== undefined ) {
-            if ( this.popupContents[popupType].formID != "" ) {
-                this.fillPopup(popupType);
-
-                $(document).trigger('popup-show', [this.popup]);
-
-                this.popup.parent().show();
-                centerVertically(this.popup);
-                if ( this.focusOnFirstInput ) {
-                    this.popup.find('input').eq(0).focus();
+        this.init = function ( settings ) {
+            if ( settings !== undefined ) {
+                for ( var setting in settings ) {
+                    this[setting] = settings[setting];
                 }
             }
-        } else {
-            console.log('showPopup');
-            console.log(popupType);
-        }
-    };
 
-    this.hidePopup = function () {
-        this.popup.parent().css('padding-top', 0);
-
-        $(document).trigger('popup-hide', [this.popup]);
-
-        this.popup.parent().hide();
-        this.popup.html('');
-    };
-
-    this.getSingleAjaxRequestData = function ( element, requestData ) {
-        if ( requestData === undefined ) {
-            requestData = {};
-        }
-        var attributeValue, quantity = 0;
-        if ( this.additionalDataAttributes !== undefined ) {
-            for ( var i = 0; i < this.additionalDataAttributes.length; i++ ) {
-                attributeValue = element.attr(this.additionalDataAttributes[i]);
-                if ( attributeValue !== undefined ) {
-                    requestData[this.additionalDataAttributes[i]] = attributeValue;
-                    quantity++;
-                }
-
-                if ( i == this.additionalDataAttributes.length - 1 && quantity == 0 ) {
-                    requestData = 0;
-                }
-            }
-        }
-
-        return requestData;
-    };
-
-    this.getAjaxRequestData = function () {
-        var ajaxRequestData = {
-            action: "ajaxGetPopupContent",
-            popupRequestData: {}
+            this.getPopupsContent();
+            this.injectPopup();
+            this.initEventListeners();
         };
 
-        var popupTriggers = $('[' + this.triggerAttribute + ']');
-        var deferredPopupTriggers = $('[' + this.deferredTriggerAttribute + ']');
+        this.getPopupsContent = function () {
 
-        ajaxRequestData = this.fillRequestData(ajaxRequestData, popupTriggers);
-        ajaxRequestData = this.fillRequestData(ajaxRequestData, deferredPopupTriggers, true);
-
-        return ajaxRequestData;
-    };
-
-    this.fillRequestData = function ( ajaxRequestData, popupTriggers, defer ) {
-        if ( defer === undefined ) {
-            defer = false;
-        }
-        var attr = !defer ? this.triggerAttribute : this.deferredTriggerAttribute;
-        var popupType, element;
-        for ( var i = 0; i < popupTriggers.length; i++ ) {
-            element = $(popupTriggers[i]);
-            popupType = element.attr(attr);
-            if ( !this.popupContents[popupType] && popupType != undefined ) {
-                ajaxRequestData.popupRequestData[popupType] = this.getSingleAjaxRequestData(element, ajaxRequestData[popupType]);
-            }
-        }
-        return ajaxRequestData;
-    };
-
-    this.updateContent = function ( contentId, newData ) {
-        if ( this.popupContents[contentId] !== undefined ) {
-            var tempContent = document.createElement('div');
-            tempContent.innerHTML = this.popupContents[contentId].content;
-            var tempContentObject = $(tempContent);
-
-            $.each(newData, function ( selector, callback ) {
-                callback(tempContentObject.find(selector), tempContentObject);
-            });
-
-            this.popupContents[contentId].content = tempContent.innerHTML;
-        } else {
-            console.log('no content');
-        }
-    };
-
-    this.injectPopup = function () {
-        if ( $('.' + this.popupClass).length == 0 ) {
-            $('body').append('<div class = "' + this.popupWrapperClass + '"><div class = "' + this.popupClass + '__close-btn"></div><div class = "' + this.popupClass + '"></div></div>');
-        }
-        this.popup = $('.' + this.popupClass);
-    };
-
-    this.initEventListeners = function () {
-        var $this = this;
-        $(document).on('click', '[' + this.triggerAttribute + ']', function ( event ) {
-            event.preventDefault();
-
-            $this.showPopup($(this).attr($this.triggerAttribute));
-
-            $($this.popupCloseSelector).click(function () {
-                $this.hidePopup();
-            });
-
-            if ( $this.closeOnWrapperClick ) {
-                $(document).click(function ( event ) {
-                    if ( $(event.target).hasClass($this.popupWrapperClass) ) {
-                        $this.hidePopup();
+            var $this = this;
+            var ajaxRequestData = this.getAjaxRequestData();
+            if ( Object.keys(ajaxRequestData.popupRequestData).length !== 0 ) {
+                $.ajax({
+                    url: themeVars.ajaxUrl,
+                    type: "POST",
+                    data: ajaxRequestData,
+                    success: function ( response ) {
+                        if ( response != "no content" ) {
+                            response = $.parseJSON(response);
+                            for ( var popupType in response ) {
+                                $this.popupContents[popupType] = {
+                                    formID: response[popupType].formID,
+                                    content: response[popupType].content
+                                };
+                            }
+                        } else {
+                            console.log(response);
+                        }
+                    },
+                    error: function ( response ) {
+                        console.log(response);
                     }
                 });
             }
-        });
-    };
+        };
+
+        this.fillPopup = function ( popupType ) {
+            var $this = this;
+            this.popup.html(this.popupContents[popupType].content);
+            this.getPopupsContent();
+            $('form#' + this.popupContents[popupType].formID).submit(function ( event ) {
+                event.preventDefault();
+                var currentForm = $(this);
+
+                if ( !currentForm.hasClass($this.disabledFormClass) ) {
+                    $this.formSubmission($this, popupType, currentForm);
+                }
+            });
+        };
+
+        this.formSubmission = function ( popupHandler, handlerType, form ) {
+            if ( this.popupHandlers[handlerType] !== undefined ) {
+                this.popupHandlers[handlerType](form, popupHandler);
+            } else {
+                console.log('formSubmission-default');
+                console.log(handlerType);
+            }
+        };
+
+        this.showPopup = function ( popupType, defer ) {
+            if ( typeof popupType !== 'string' ) {
+                defer = defer === undefined ? false : defer;
+                var attr = defer ? this.deferredTriggerAttribute : this.triggerAttribute;
+                popupType = popupType.attr(attr);
+            }
+            this.hidePopup();
+
+            if ( this.popupContents[popupType] !== undefined ) {
+                if ( this.popupContents[popupType].formID != "" ) {
+                    this.fillPopup(popupType);
+
+                    $(document).trigger('popup-show', [this.popup]);
+
+                    this.popup.parent().show();
+                    centerVertically(this.popup);
+                    if ( this.focusOnFirstInput ) {
+                        this.popup.find('input').eq(0).focus();
+                    }
+                }
+            } else {
+                console.log('showPopup');
+                console.log(popupType);
+            }
+        };
+
+        this.hidePopup = function () {
+            this.popup.parent().css('padding-top', 0);
+
+            $(document).trigger('popup-hide', [this.popup]);
+
+            this.popup.parent().hide();
+            this.popup.html('');
+        };
+
+        this.getSingleAjaxRequestData = function ( element, requestData ) {
+            if ( requestData === undefined ) {
+                requestData = {};
+            }
+            var attributeValue, quantity = 0;
+            if ( this.additionalDataAttributes !== undefined ) {
+                for ( var i = 0; i < this.additionalDataAttributes.length; i++ ) {
+                    attributeValue = element.attr(this.additionalDataAttributes[i]);
+                    if ( attributeValue !== undefined ) {
+                        requestData[this.additionalDataAttributes[i]] = attributeValue;
+                        quantity++;
+                    }
+
+                    if ( i == this.additionalDataAttributes.length - 1 && quantity == 0 ) {
+                        requestData = 0;
+                    }
+                }
+            }
+
+            return requestData;
+        };
+
+        this.getAjaxRequestData = function () {
+            var ajaxRequestData = {
+                action: "ajaxGetPopupContent",
+                popupRequestData: {}
+            };
+
+            var popupTriggers = $('[' + this.triggerAttribute + ']');
+            var deferredPopupTriggers = $('[' + this.deferredTriggerAttribute + ']');
+
+            ajaxRequestData = this.fillRequestData(ajaxRequestData, popupTriggers);
+            ajaxRequestData = this.fillRequestData(ajaxRequestData, deferredPopupTriggers, true);
+
+            return ajaxRequestData;
+        };
+
+        this.fillRequestData = function ( ajaxRequestData, popupTriggers, defer ) {
+            if ( defer === undefined ) {
+                defer = false;
+            }
+            var attr = !defer ? this.triggerAttribute : this.deferredTriggerAttribute;
+            var popupType, element;
+            for ( var i = 0; i < popupTriggers.length; i++ ) {
+                element = $(popupTriggers[i]);
+                popupType = element.attr(attr);
+                if ( !this.popupContents[popupType] && popupType != undefined ) {
+                    ajaxRequestData.popupRequestData[popupType] = this.getSingleAjaxRequestData(element, ajaxRequestData[popupType]);
+                }
+            }
+            return ajaxRequestData;
+        };
+
+        this.updateContent = function ( contentId, newData ) {
+            if ( this.popupContents[contentId] !== undefined ) {
+                var tempContent = document.createElement('div');
+                tempContent.innerHTML = this.popupContents[contentId].content;
+                var tempContentObject = $(tempContent);
+
+                $.each(newData, function ( selector, callback ) {
+                    callback(tempContentObject.find(selector), tempContentObject);
+                });
+
+                this.popupContents[contentId].content = tempContent.innerHTML;
+            } else {
+                console.log('no content');
+            }
+        };
+
+        this.injectPopup = function () {
+            if ( $('.' + this.popupClass).length == 0 ) {
+                $('body').append('<div class = "' + this.popupWrapperClass + '"><div class = "' + this.popupClass + '__close-btn"></div><div class = "' + this.popupClass + '"></div></div>');
+            }
+            this.popup = $('.' + this.popupClass);
+        };
+
+        this.initEventListeners = function () {
+            var $this = this;
+            $(document).on('click', '[' + this.triggerAttribute + ']', function ( event ) {
+                event.preventDefault();
+
+                $this.showPopup($(this).attr($this.triggerAttribute));
+
+                $($this.popupCloseSelector).click(function () {
+                    $this.hidePopup();
+                });
+
+                if ( $this.closeOnWrapperClick ) {
+                    $(document).click(function ( event ) {
+                        if ( $(event.target).hasClass($this.popupWrapperClass) ) {
+                            $this.hidePopup();
+                        }
+                    });
+                }
+            });
+        };
+    } else {
+        return new PopupHandler();
+    }
 }
